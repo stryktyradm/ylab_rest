1202aea061fc8b48c430ec074216d6f775baa36f
Daniil Mikhailov <stryktyradm@gmail.com>
2023-07-30 23:29:31 +0300

    fix root

--- app/src/tests/api/test_root.py
+++ app/src/tests/api/test_root.py
@@ -4,14 +4,16 @@ from sqlalchemy.orm import Session
 
 
 def test_root(client: TestClient, get_db: Session):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_present_tables(get_db: Session):
     db_tables = ['menu', 'submenu', 'dish']
     test_query = text(
         "SELECT tablename FROM pg_catalog.pg_tables WHERE schemaname = 'public';"
     )
     rows = get_db.execute(test_query)
-    response = client.get("/")
-    assert response.status_code == 200
     fetched_tables = [row[0] for row in rows.fetchall()]
     for table_name in db_tables:
         assert table_name in fetched_tables
-
